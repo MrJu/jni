1f0f50c0d180ad695ce9ada9577053fa0679ff4a
andrew <mrju.email@gmail.com>
2019-11-07 09:07:09 +0800

    int array 2nd
    Signed-off-by: andrew <mrju.email@gmail.com>

---
+++ int_array_2nd/native.c
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2019 Andrew <mrju.email@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ */
+
+#include <jni.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+JNIEXPORT jint JNICALL Java_Foo_nativeFoo(
+	JNIEnv *env, jobject jobj, jintArray jarr)
+{
+	jint i, size;
+	jint *arr;
+	jint *temp;
+
+	size = (*env)->GetArrayLength(env, jarr);
+	temp = malloc(sizeof(jint) * size);
+	if (!temp)
+		return JNI_ERR;
+
+	for (i = 0; i < size; i++)
+		temp[i] = i;
+
+	(*env)->SetIntArrayRegion(env, jarr, 0, size, temp);
+	free(temp);
+
+	return 0;
+}
+
+JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *jvm, void *reserved)
+{
+	JNIEnv *env;
+	jclass class;
+
+	if ((*jvm)->GetEnv(jvm, (void **)&env, JNI_VERSION_1_4))
+		return JNI_ERR;
+
+	return JNI_VERSION_1_4;
+}
