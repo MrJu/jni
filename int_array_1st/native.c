49227552338f0f2e6a639ff5be4fb67875622546
andrew <mrju.email@gmail.com>
2019-11-07 08:55:53 +0800

    int array 1st
    Signed-off-by: andrew <mrju.email@gmail.com>

---
+++ int_array_1st/native.c
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2019 Andrew <mrju.email@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ */
+
+#include <jni.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+JNIEXPORT jint JNICALL Java_Foo_nativeFoo(
+	JNIEnv *env, jobject jobj, jintArray jarr)
+{
+	jint i, size;
+	jint *arr;
+
+	arr = (*env)->GetIntArrayElements(env, jarr, 0);
+	if (!arr)
+		return JNI_ERR;
+
+	size = (*env)->GetArrayLength(env, jarr);
+
+	for (i = 0; i < size; i++)
+		printf("%s: %d %d\n", __func__, __LINE__, arr[i]);
+
+	(*env)->ReleaseIntArrayElements(env, jarr, arr, 0);
+
+	return 0;
+}
+
+JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *jvm, void *reserved)
+{
+	JNIEnv *env;
+	jclass class;
+
+	if ((*jvm)->GetEnv(jvm, (void **)&env, JNI_VERSION_1_4))
+		return JNI_ERR;
+
+	return JNI_VERSION_1_4;
+}
